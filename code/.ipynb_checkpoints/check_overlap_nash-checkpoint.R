d66f0ffb46497419088dc5553663ea45f9569a56
jerome1993@qq.com <jerome1993@qq.com>
2019-10-31 17:14:41 -0400

    update

---
+++ code/.ipynb_checkpoints/check_overlap_nash-checkpoint.R
@@ -0,0 +1,20 @@
+# check overlap
+drug_str <- "ACARBOSE AMLODIPINE ATORVASTATIN BETAINE CAFFEINE CENICRIVIROC CHOLECALCIFEROL DABIGATRAN DAPAGLIFLOZIN EMRICASAN ERGOCALCIFEROL EXENATIDE EZETIMIBE FENOFIBRATE GLIMEPIRIDE INSULIN GLARGINE INSULIN HUMAN PRADIGASTAT K-877 PX-102 LIRAGLUTIDE QUINIDINE LOSARTAN ROFLUMILAST METFORMIN ROSIGLITAZONE METRELEPTIN ROSUVASTATIN MGL-3196 SELADELPAR MIDAZOLAM SELONSERTIB SEMAGLUTIDE MT-3995 MUROMONAB-CD3 SIMTUZUMAB NAMODENOSON SITAGUPTIN NIVOCASAN SPIRONOLACTONE OBETICHOLIC ACID TELMISARTAN PENTOXIFYLUNE TOFOGLIFLOZIN PERINDOPRIL URSODIOL PIOGLITAZONE"
+
+nash_drugs <- tolower(unlist(stringr::str_split(drug_str, " ")))
+
+check_overlap <- function(gsea_df, nash_drugs) {
+    lincs_drugs <- tolower(gsea_df$compound_name)
+
+    midx <- na.omit(match(nash_drugs, lincs_drugs))
+    overlapped_drugs <- lincs_drugs[midx]
+    sum_overlap <- ifelse(lincs_drugs %in% overlapped_drugs, 1, 0)
+    #sum_overlap_top5 <- ifelse(lincs_drugs[(5774-289+1):5774] %in% overlapped_drugs, 1, 0)
+    sum_overlap_top5 <- ifelse(lincs_drugs[1:289] %in% overlapped_drugs, 1, 0)
+    #sum_overlap_top10 <- ifelse(lincs_drugs[(5774-578+1):5774] %in% overlapped_drugs, 1, 0)
+    sum_overlap_top10 <- ifelse(lincs_drugs[1:578] %in% overlapped_drugs, 1, 0)
+    sum_overlap_top_all <- ifelse(lincs_drugs %in% overlapped_drugs, 1, 0)
+    return(c(sum(sum_overlap_top5), sum(sum_overlap_top10)))
+}
+
+
--- code/ks_connectivity.R
+++ code/ks_connectivity.R
@@ -9,8 +9,8 @@ library(dplyr)
     return( 0 )
   } else {
     
-    if ( is.unsorted( V ) )
-      V <- sort( V )
+    #if ( is.unsorted( V ) )
+    #  V <- sort( V )
     d <- (1:t) / t - V / n
     a <- max( d )
     b <- -min( d ) + 1 / t
@@ -18,9 +18,31 @@ library(dplyr)
   }
 }
 
+.ks_mean <- function( V, n ) {
+  t <- length( V )
+  
+  if( t == 0 )  {
+    return( 0 )
+  } else {
+    
+    #if ( is.unsorted( V ) )
+    #  V <- sort( V )
+    d <- (1:t) / t - V / n
+    a <- mean( d )
+    b <- -mean( d ) + 1 / t
+    ifelse( a > b, a, -b )
+  }
+}
+
 .s <- function( V_up, V_down, n ) {
   ks_up <- .ks( V_up, n )
   ks_down <- .ks( V_down, n )
+  ifelse( sign( ks_up ) == sign( ks_down ), 0, ks_up - ks_down )
+}
+
+.s_mean <- function( V_up, V_down, n ) {
+  ks_up <- .ks_mean( V_up, n )
+  ks_down <- .ks_mean( V_down, n )
   ifelse( sign( ks_up ) == sign( ks_down ), 0, ks_up - ks_down )
 }
 
@@ -50,6 +72,28 @@ ks_score <- function(up_signature, down_signature, rank_matrix, compound_name) {
         up_v <- match(up_signature, pert_vec)
         down_v <- match(down_signature, pert_vec)
         tmp_s <- .s(up_v, down_v, nrow(gene_rank_bing))
+        connectivity_score_vec <- c(connectivity_score_vec, tmp_s)
+        
+    }
+    output_df <- data.frame(compound_name = compound_name, connectivity_score = connectivity_score_vec)
+    return(output_df)
+    
+}
+
+# main function ks score
+ks_score_mean <- function(up_signature, down_signature, rank_matrix, compound_name) {
+    n = 1
+    connectivity_score_vec <- c()
+    gene_rank_bing <- rank_matrix
+    for (i in 1:ncol(gene_rank_bing)) {
+        if (i %% 500 == 0)  { print(paste0("INFO: ", i, " Instances."))}
+        pert_vec <- gene_rank_bing[, i]
+        up_signature <- up_signature[up_signature %in% pert_vec]   
+        down_signature <- down_signature[down_signature %in% pert_vec]
+        # get teh rank
+        up_v <- match(up_signature, pert_vec)
+        down_v <- match(down_signature, pert_vec)
+        tmp_s <- .s_mean(up_v, down_v, nrow(gene_rank_bing))
         connectivity_score_vec <- c(connectivity_score_vec, tmp_s)
         
     }
